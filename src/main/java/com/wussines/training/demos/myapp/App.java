09f2a6578f3b032b08655d6c6ef9f98ef99b8715
wilmar <wil.pac510@gmail.com>
2021-08-28 15:00:00 -0500

    Empezando con los patrones proxys

--- src/main/java/com/wussines/training/demos/myapp/App.java
+++ src/main/java/com/wussines/training/demos/myapp/App.java
@@ -1,14 +1,9 @@
  package com.wussines.training.demos.myapp;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 public class App {
 
 	public static void main(String[] args) {
-		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
-		MyService bean = ctx.getBean(MyService.class);
-		bean.doBusinessLogic();
+		System.out.println("Hola soy una nueva rama para los patrones empezando con el Proxy...");
 	}
 
 }

c19cfb5e32639db7f18a977d073cf7d950411f74
wilmar <wil.pac510@gmail.com>
2021-08-28 04:00:49 -0500

    Leyendo la Injection properties desde un archivo con values

--- src/main/java/com/wussines/training/demos/myapp/business/MyServiceImpl.java
+++ src/main/java/com/wussines/training/demos/myapp/business/MyServiceImpl.java
@@ -1,10 +1,15 @@
 package com.wussines.training.demos.myapp.business;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import com.wussines.training.demos.myapp.data.MyRepository;
 
 @Service
 public class MyServiceImpl implements MyService {
+	
+	@Value("${my.name}")
+	private String name;
+	
 	
 	private MyRepository repository;
 	
@@ -14,7 +19,7 @@ public class MyServiceImpl implements MyService {
 
 	@Override
 	public void doBusinessLogic() {
-		System.out.println("Doing business logic");
+		System.out.println("Doing business logic " + name);
 		repository.doQuery();
 	}
 }
--- src/main/java/com/wussines/training/demos/myapp/config/AppConfig.java
+++ src/main/java/com/wussines/training/demos/myapp/config/AppConfig.java
@@ -2,15 +2,16 @@ package com.wussines.training.demos.myapp.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.Scope;
 
 import com.wussines.training.demos.myapp.business.MyService;
 import com.wussines.training.demos.myapp.business.MyServiceImpl;
 import com.wussines.training.demos.myapp.data.MyRepository;
-import com.wussines.training.demos.myapp.data.MyRepositoryImpl;
 import com.wussines.training.demos.myapp.data.OtherRepository;
 
 @Configuration
+@PropertySource("classpath:application.properties")
 public class AppConfig {
 	
 	@Bean

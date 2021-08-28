93223443bb7f240a23ba5c2c4db06ffeb290a920
wilmar <wil.pac510@gmail.com>
2021-08-28 02:37:34 -0500

    Cambiando configuración a Java Configuration Class

--- src/main/java/com/wussines/training/demos/myapp/App.java
+++ src/main/java/com/wussines/training/demos/myapp/App.java
@@ -3,10 +3,12 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import com.wussines.training.demos.myapp.config.AppConfig;
+
 public class App {
 
 	public static void main(String[] args) {
-		ApplicationContext ctx = new AnnotationConfigApplicationContext("com.wussines.training.demos.myapp");
+		ApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
 		MyService bean = ctx.getBean(MyService.class);
 		bean.doBusinessLogic();
 		
---
+++ src/main/java/com/wussines/training/demos/myapp/config/AppConfig.java
@@ -0,0 +1,10 @@
+package com.wussines.training.demos.myapp.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.wussines.training.demos.myapp")
+public class AppConfig {
+	
+}
